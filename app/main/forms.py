aefa972e6ec2de89b4eb0782c156438144ac5e43
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-06-03 14:07:43 +0300

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,15 +9,7 @@ from ..models import Role, User
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-
-class EditProfileForm(FlaskForm):
-    name = StringField('Real name', validators=[Length(0, 64)])
-    location = StringField('Location', validators=[Length(0, 64)])
-    about_me = TextAreaField('About me')
-    submit = SubmitField('Submit')
-
+    submit = SubmitFiel
 
 class EditProfileAdminForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
