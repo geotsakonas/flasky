f25e519608eac75f5d81869f2153f71c842fa15f
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-06-14 11:48:39 +0300

    Update decorators.py

--- app/decorators.py
+++ app/decorators.py
@@ -7,11 +7,7 @@ from .models import Permission
 def permission_required(permission):
     def decorator(f):
         @wraps(f)
-        def decorated_function(*args, **kwargs):
-            if not current_user.can(permission):
-                abort(403)
-            return f(*args, **kwargs)
-        return decorated_function
+        def decorated_functiion
     return decorator
 
 
